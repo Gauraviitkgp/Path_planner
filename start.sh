0a036d0ab5ffe23cd25991e0b13789e4078cf2e1
Gauraviitkgp <gauravs123456789@gmail.com>
2018-10-27 20:23:00 +0100

    Added .sh file

---
+++ start.sh
@@ -0,0 +1,8 @@
+# sudo systemctl start firmwared.service
+# sudo firmwared
+# # New tab
+
+# sphinx /opt/parrot-sphinx/usr/share/sphinx/drones/bebop2.drone 
+# sudo usermod -a -G firmwared slytherin
+# newgrp firwared
+source src/px4_gaz/iarc_sim_run.sh
